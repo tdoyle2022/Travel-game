ff4ec5b76ff2d9c5e3bcd0439103c5fe4c308f9f
Terry Doyle <terrence_doyle@hotmail.com>
2023-04-28 11:21:12 -0500

    PROJECT-010: Trying to update main and work through merge conflicts

--- travelquiz_proj/user_app/views.py
+++ travelquiz_proj/user_app/views.py
@@ -1,14 +1,9 @@
-from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate, login, logout
 from .models import App_User 
 from .utilities import sign_up, log_in, curr_user
 from django.core.serializers import serialize
-import json
-from django.shortcuts import redirect
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 
 # Create your views here.
 
@@ -33,14 +28,8 @@ def user_capabilities(request):
     elif request.method == 'GET':
         return curr_user(request)
         
-    
-    
 def send_the_index(request):
     the_index = open('static/index.html')
     return HttpResponse(the_index)
 
-# @api_view(['DELETE'])
-# def delete_user(request):
-#     user = get_object_or_404(App_User)
-#     user.delete()
-#     return Response(status=204)
+
